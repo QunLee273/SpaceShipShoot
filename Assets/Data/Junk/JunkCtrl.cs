5c397b9abdf8b9c683cc187282a0979481886d3a
QunLee273 <quan2732k3@gmail.com>
2023-10-02 14:39:47 +0700

    Quan

---
+++ Assets/Data/Junk/JunkCtrl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunkCtrl : ShipMonoBehaviour
+{
+    [SerializeField] protected Transform model;
+    public Transform Model { get => model; }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadModel();
+    }
+
+    protected virtual void LoadModel()
+    {
+        if (this.model != null) return;
+        this.model = transform.Find("Model");
+        Debug.Log(transform.name + ": LoadModel", gameObject);
+    }
+}
